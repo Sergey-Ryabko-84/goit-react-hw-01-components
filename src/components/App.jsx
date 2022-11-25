a19506b71a021d5847625f501a5e87d0f53fe405
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-11-25 03:21:18 +0200

    Adds React components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friend-list/FriendList';
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+import user from './profile/user.json';
+import data from './statistics/data.json';
+import friends from './friend-list/friends.json';
+import transactions from './transaction-history/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    {/*-------- 1 - Social network profile --------*/}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    {/*-------- 2 - Statistics section -------------*/}
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+    {/*-------- 3 - List of friends ----------------*/}
+      <FriendList friends={friends} />;
+    {/*-------- 4 - History of transactions --------*/}
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
---
+++ src/components/friend-list/FriendListItem.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+  return (
+    <>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </>
+  );
+};
+
+FriendListItem.protoTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
